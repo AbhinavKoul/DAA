9cbf62c5ee8b7515599911a29e5c295f51a111ac
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2020-05-29 00:40:12 +0530

    bubblesort updare

--- bubblesort.c
+++ bubblesort.c
@@ -2,69 +2,39 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#include<stdbool.h>
-#define size 150000
 
-
-int bsort(int a[],int n)
+void bubble_sort(int a[],int size)
 {
 	int i,j,temp;
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<n-1-i;j++)
-		{
-			if(a[j]>a[j+1])
+	for(i=0;i<size-1;i++)
+		for(j=0;j<size-1-i;j++)
+			if(a[j] > a[j+1])
 			{
-				temp=a[j+1];
-				a[j+1]=a[j];
-				a[j]=temp;
+				temp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = temp;
 			}
-		}
-	}
-	
 }
 
 void main()
 {
-	int a[size];
-	int n,i,j,temp;
-	time_t start,end;
-	bool arr[size]={0};
-	int count=0;
+	int i,size;
+	printf("Enter the Size of array\t:");
+	scanf("%d",&size);
+	int arr[size];
+	//input elements
+	for(i=0;i<size;i++)
+		arr[i] = rand()%100;
+	//initializing clock variables
+	clock_t start,end;
+	double time;
+	start = clock();
+	bubble_sort(arr,size);
+	end = clock();
+	time = (double)(end-start)/CLOCKS_PER_SEC; 
+	printf("Sorted arrat is\n:");
+	for(i=0;i<size;i++)	
+		printf("%d    ",arr[i]);
+	printf("\ntime taken : %f",time); 
 
-	printf("Enter number of array elements\n");
-	scanf("%d",&n);
-
-	printf("Enter array elements one by one\n");
-
-	srand(time(0));	
-
-	for(i=0;i<n;i++)
-	{	a[i]=rand()%140000+1;
-		if(!arr[a[i]])
-			{arr[a[i]]=1;
-				count++;}
-		else
-			i--;
-		
-	}
-	/*for(i=0;i<n;i++)
-		scanf("%d",&a[i]);*/
-	printf("Array elements before sorting\n");
-	for(i=0;i<n;i++)
-		printf("%d\t",a[i]);
-	
-	start=clock();
-	bsort(a,n);
-	end=clock();
-
-	printf("Array elements after sorting\n");
-	for(i=0;i<n;i++)
-		printf("%d\n",a[i]);
-
-	double timet;
-	timet=(double)(end-start)/CLOCKS_PER_SEC;
-
-	printf("\nTime:%lf\n",timet);
-	printf("\nNUmber:%d\n",count);
 }
